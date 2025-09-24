91965d95b41a5db104082cb37251b6847cc9a3fa
Charles-Henri Moser <pc01zcg@eduvaud.ch>
2025-09-24 08:56:42 +0200

    feat(Rando): Refaire Exercise

---
+++ PERSONNEL_CHM/exercices/randoOld/Rando/Rando/Trackpoint.cs
@@ -0,0 +1,10 @@
+﻿namespace Rando
+{
+    public class Trackpoint
+    {
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public double Elevation { get; set; } // meters
+        public DateTime? Time { get; set; } // optional
+    }
+}
